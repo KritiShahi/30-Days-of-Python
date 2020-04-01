7f3c69a8ccd02edb9e2c14532fe19564aa34d43e
Kriti Shahi <kritishahi66@gmail.com>
2020-04-01 23:51:16 +0530

    Completed Day 1 Python Excercise

---
+++ Day 1.py
@@ -0,0 +1,14 @@
+# Day 1 - Performing arithmetic operations on numbers and printing the results
+
+# Q1 - Print your age to console
+print(21)
+
+# Q2 - Calculate and print the number of days, weeks, and months in 27 years without taking the account of leap years
+print(27 * 365)
+print(27 * 52)
+print(27 * 12)
+
+# Q3 - Calculate and print the area of a circle with a radius of 5 units
+print(3.14 * 5 * 5)         # First Approach
+print(3.14 * 5 ** 2)        # Second Approach
+print(3.14 * pow(5, 2))     # Third Approach
